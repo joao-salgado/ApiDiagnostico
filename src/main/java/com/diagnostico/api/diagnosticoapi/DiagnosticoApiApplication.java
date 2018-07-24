338fb0f359d50ba51d293cae6eb7f667f391bb7f
João Salgado <jgs1884@gmail.com>
2018-07-23 21:32:44 -0300

    Initial commit

---
+++ src/main/java/com/diagnostico/api/diagnosticoapi/DiagnosticoApiApplication.java
@@ -0,0 +1,12 @@
+package com.diagnostico.api.diagnosticoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosticoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnosticoApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/diagnostico/api/diagnosticoapi/DiagnosticoApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.diagnostico.api.diagnosticoapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiagnosticoApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
