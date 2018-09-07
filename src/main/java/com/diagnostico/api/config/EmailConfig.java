7e785669411f38b1e112100dd52bcafcfac63c87
João Salgado <jgs1884@gmail.com>
2018-09-07 17:11:05 -0300

    email credentials

--- src/main/java/com/diagnostico/api/config/EmailConfig.java
+++ src/main/java/com/diagnostico/api/config/EmailConfig.java
@@ -2,36 +2,31 @@ package com.diagnostico.api.config;
 
 import java.util.Properties;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-import com.diagnostico.api.config.property.DiagnosisApiProperty;
-
 @Configuration
 public class EmailConfig {
 
-	@Autowired
-	private DiagnosisApiProperty property;
+	/*@Autowired
+	private DiagnosisApiProperty property;*/
 
 	@Bean
 	public JavaMailSender javaMailSender() {
 		
-		Properties props = new Properties();
-		props.put("mail.transport.protocol", "smtp");
-		props.put("mail.smtp.auth", true);
-		props.put("mail.smtp.starttls.enable", true);
-		props.put("mail.smtp.connectiontimeout", 10000);
-		props.put("mail.debug", "true");
-		
 		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-		mailSender.setJavaMailProperties(props);
-		mailSender.setHost(property.getMail().getHost());
-		mailSender.setPort(587);
-		mailSender.setUsername(property.getMail().getUsername());
-		mailSender.setPassword(property.getMail().getPassword());
+	    mailSender.setHost("smtp.gmail.com");
+	    mailSender.setPort(587);
+	    mailSender.setUsername("diagnosticoappcontato@gmail.com");
+	    mailSender.setPassword("fnpnukrxzhoehlds");
+	     
+	    Properties props = mailSender.getJavaMailProperties();
+	    props.put("mail.transport.protocol", "smtp");
+	    props.put("mail.smtp.auth", "true");
+	    props.put("mail.smtp.starttls.enable", "true");
+	    props.put("mail.debug", "true");
 		 
 	    return mailSender;
 	}
