f48d9009454a72c3c2baf7060b5be0b50bba98a2
João G Salgado <joao@maptriz.com.br>
2019-01-19 09:58:50 -0200

    update company process

--- src/main/resources/db/migration/V14__add_news_companies_process.sql
+++ src/main/resources/db/migration/V14__add_news_companies_process.sql
@@ -4,9 +4,3 @@ INSERT INTO company_process(name) VALUES ('XP'),
 											('DSDM'),
 											('Não é utilizado');
 											
-UPDATE company
-SET company_process_id = 11
-WHERE company_process_id = 2;
-
-DELETE FROM company_process 
-WHERE id = 2;
